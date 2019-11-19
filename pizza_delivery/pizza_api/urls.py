17c7f42b4c1d2c20b469e7f9c71725375b2ec0f5
RobertKappler <robert@kapplers.de>
2019-11-19 11:27:56 +0100

    update urls

--- pizza_delivery/pizza_api/urls.py
+++ pizza_delivery/pizza_api/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orders/', views.orders_list),
-    path('customer_details/<uuid:customer_id>/', views.customer_details),
-    path('remove_order/<uuid:order_id>/', views.remove_order),
-    path('insert_order/', views.insert_order)
+    path('orders', views.OrderList.as_view()),
+    path('order/<uuid:orderid>', views.OrderDetails.as_view()),
+    path('order', views.OrderDetails.as_view())
 ]
