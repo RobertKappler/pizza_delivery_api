193fa1dc7126cdbf707a9c77b93e73209518c856
RobertKappler <robert@kapplers.de>
2019-11-19 11:29:23 +0100

    update views for basic retrieve create update delete functions

--- pizza_delivery/pizza_api/views.py
+++ pizza_delivery/pizza_api/views.py
@@ -1,67 +1,160 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.generics import ListAPIView
+from .models import Orders, Customer
+from .serializers import OrdersSerializier, CustomerSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from django.http import Http404
 
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from .models import Orders, Item, Customer
-from .serializers import OrdersSerializier, ItemSerializer, CustomerSerializer
 
-
-@csrf_exempt
-def orders_list(request):
+class OrderList(ListAPIView):
     """List all orders."""
+    serializer_class = OrdersSerializier
 
-    if request.method == 'GET':
-        orders = Orders.objects.select_related().all()
-        serializer = OrdersSerializier(orders, many=True)
+    def get_queryset(self):
+        """Define queryset to enabling filtering on customer_id and status."""
+
+        queryset = Orders.objects.all()
+        query_status = self.request.query_params.get('status', None)
+        query_customer_id = self.request.query_params.get('customer_id', None)
+        if query_status and query_customer_id:
+            queryset = Orders.objects.filter(status=query_status, customer_id=query_customer_id)
+        elif query_status:
+            queryset = Orders.objects.filter(status=query_status)
+        elif query_customer_id:
+            queryset = Orders.objects.filter(customer_id=query_customer_id)
+        return queryset
+
+
+class OrderDetails(APIView):
+    """Retrieve, update or delete an order."""
+
+    @staticmethod
+    def get_order(orderid):
+        """Lookup if order details are available, based on orderid."""
+        try:
+            return Orders.objects.get(pk=orderid)
+        except Orders.DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_order_by_customerid(customerid):
+        """Lookup if order details are available, based on customerid."""
+
+        try:
+            return Orders.objects.get(customer_id=customerid)
+        except Orders.DoesNotExist:
+            return None
+        except Orders.MultipleObjectsReturned:
+            return True
+
+    def get(self, request, orderid):
+        """Retrieve order details."""
+        order = self.get_order(orderid)
+        serializer = OrdersSerializier(order)
         return JsonResponse(serializer.data, safe=False)
 
+    def put(self, request):
+        """Update order details, all or partial."""
 
-@csrf_exempt
-def customer_details(request, customer_id):
-    """Retrieve specific customer details."""
+        all_data = request.data
 
-    if request.method == 'GET':
-        customer = Customer.objects.select_related().get(pk=customer_id)
-        serializer = CustomerSerializer(customer, many=False)
-        return JsonResponse(serializer.data, safe=False)
+        existing_order = None
+        # check status of order if id is given
+        if 'id' in all_data.keys():
+            existing_order = self.get_order(all_data['id'])
+            if existing_order.status > 1:
+                return JsonResponse({'Error': 'Can`t change orderdetails becauser the state of the '
+                                              'delivery process too advanced'})
 
+        # get customer_id out of customer details - create new or get existing
+        elif 'customer' in all_data.keys():
+            cus_data = all_data['customer']
+            existing_cus = CustomerDetails().get_customer_by_name_and_address(cus_data['lastname'], cus_data['name'],
+                                                                  cus_data['address'])
+            if existing_cus:
+                all_data['customer_id'] = existing_cus.id
+            else:
+                cus_serializer = CustomerSerializer(None, data=all_data['customer'], partial=True)
+                if cus_serializer.is_valid():
+                    saved_cus = cus_serializer.save()
+                    all_data['customer_id'] = saved_cus.id
 
-@csrf_exempt
-def insert_order(request):
-    """Insert a new order."""
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = OrdersSerializier(data=data)
+        # check its a new order and no customer information are provided
+        elif 'customer_id' not in all_data.keys():
+            return JsonResponse({'Error': 'Can`t create an order without any customer information are given. Please '
+                                          'enter customer information or a customer_id.'})
+
+        serializer = OrdersSerializier(existing_order, data=all_data, partial=True)
+
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+    def delete(self, request, orderid):
+        """Delete order details and check if these is the last order for this customer."""
+
+        order = self.get_order(orderid)
+        if not self.get_order_by_customerid(order.customer_id):
+            CustomerDetails().delete({}, order.customer_id)
+        order.delete()
+        return JsonResponse(status.HTTP_204_NO_CONTENT, safe=False)
 
 
-@csrf_exempt
-def remove_order(request, order_id):
-    if request.method == 'POST':
-        order = Orders.objects.select_related().get(pk=order_id)
-        print(order)
-        serializer = OrdersSerializier(order, many=False)
-        return JsonResponse(serializer.data, safe=False)
+class CustomerDetails(APIView):
+    """Retrieve, update or delete an order."""
 
+    @staticmethod
+    def get_object(customerid):
+        """Lookup if customer details are available, based on customerid."""
 
-def retrieve_all_orders(request):
-    """"""
-    # TODO return all orders
-    # geht nicht
-    all_order = Orders.objects.select_related().all()
-    print(all_order[0].id)
-    all_items = Item.objects.filter(order_id=all_order[0].id).all()
-    print(all_items)
-    #all_order = {}
-    return HttpResponse(f'{all_order}')
+        try:
+            return Customer.objects.get(pk=customerid)
+        except Customer.DoesNotExist:
+            raise Http404
 
+    def get_customer_by_name_and_address(self, lastname, name, address):
+        """Lookup if customer details are already available, based on lastname, name, address."""
 
-def update_order(request, id, new_order_details):
-    # TODO update just when not delivered/cooking
-    # TODO check which details should be changed and change just them!
-    updated_order = {}
-    return HttpResponse(f'{updated_order}')
+        try:
+            return Customer.objects.get(lastname=lastname, name=name, address=address)
+        except Customer.DoesNotExist:
+            return None
+
+    def get(self, request, customerid):
+        """Retrieve customer details."""
+
+        customer = self.get_object(customerid)
+        serialzier = CustomerSerializer(customer)
+        return JsonResponse(serialzier.data, safe=False)
+
+    def put(self, request, customerid):
+        """Update customer details, all or partial."""
+
+        customer = self.get_object(customerid)
+        serializer = CustomerSerializer(customer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+    def delete(self, request, customerid):
+        """Delete customer details."""
+
+        customer = self.get_object(customerid)
+        customer.delete()
+        return JsonResponse(status.HTTP_204_NO_CONTENT, safe=False)
+
+    def post(self, request):
+        """Insert new customer details."""
+
+        lastname = request.data.get('lastname', None)
+        name = request.data.get('name', None)
+        address = request.data.get('address', None)
+        customer = self.get_customer_by_name_and_address(lastname, name, address)
+        serializer = CustomerSerializer(customer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
