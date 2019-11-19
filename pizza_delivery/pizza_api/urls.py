0756fda9ae48125688ff9345ec0b64f908fd0a05
RobertKappler <robert@kapplers.de>
2019-11-19 13:01:20 +0100

    add name to the urls for testing

--- pizza_delivery/pizza_api/urls.py
+++ pizza_delivery/pizza_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orders', views.OrderList.as_view()),
+    path('orders', views.OrderList.as_view(), name='orders'),
     path('order/<uuid:orderid>', views.OrderDetails.as_view()),
-    path('order', views.OrderDetails.as_view())
+    path('order', views.OrderDetails.as_view(), name='order')
 ]
